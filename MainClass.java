ef585f167f9301ed112b65161e3f0975cac1050e
yunitaasiagian22 <57378771+yunitaasiagian22@users.noreply.github.com>
2019-11-05 11:42:10 +0700

    Version 1.6

--- MainClass.java
+++ MainClass.java
@@ -3,8 +3,8 @@ import java.io.*;
 
 public class MainClass {
 	public static void main(String[] args) {
-		System.out.println("This is Version 1.5");
-		System.out.println("Welcome to Version 1.5");
+		System.out.println("This is Version 1.6");
+		System.out.println("Welcome to Version 1.6");
 		
 		
 	}
