1666e00368391b51f167ee677824ad5f51bfb64c
dandles <dandles.banjarnahor@gmail.com>
2019-11-05 13:56:15 +0700

    Version 1.9 add

--- MainClass.java
+++ MainClass.java
@@ -8,7 +8,7 @@ public class MainClass {
 		System.out.println("Welcome to Version 1.6");
 =======
 		System.out.println("This is Version 1.8");
-		System.out.println("Welcome to Version 1.8");
+		System.out.println("GAK JELAS");
 >>>>>>> 2fe43895da1ea877653f89e9f4cbf12fe5943c13
 		
 		System.out.println("Hallo");
