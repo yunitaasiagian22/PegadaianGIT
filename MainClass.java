b269a2a8440cee9b9f37a6117eb6495d0c92ff30
yunitaasiagian22 <57378771+yunitaasiagian22@users.noreply.github.com>
2019-11-05 10:53:12 +0700

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -3,8 +3,13 @@ import java.io.*;
 
 public class MainClass {
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println("This is Version 1.6");
 		System.out.println("Welcome to Version 1.6");
+=======
+		System.out.println("This is Version 1.5");
+		System.out.println("Welcome to Version 1.5");
+>>>>>>> parent of 461d3b5... Version 1.7
 		
 		
 	}
