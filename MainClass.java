4499f082cdb7ef35bc51ae2cf4b5c6a2b45979d8
yunitaasiagian22 <57378771+yunitaasiagian22@users.noreply.github.com>
2019-11-05 09:58:20 +0700

    Create MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+package latihanGIT;
+import java.io.*;
+
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("This is Version 1.1");
+		System.out.println("Welcome to Version 1.1");
+		
+		
+	}
+
+}
