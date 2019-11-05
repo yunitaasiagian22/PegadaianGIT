2fe43895da1ea877653f89e9f4cbf12fe5943c13
yunitaasiagian22 <57378771+yunitaasiagian22@users.noreply.github.com>
2019-11-05 10:56:20 +0700

    Version 1.8

--- MainClass.java
+++ MainClass.java
@@ -3,13 +3,8 @@ import java.io.*;
 
 public class MainClass {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("This is Version 1.6");
-		System.out.println("Welcome to Version 1.6");
-=======
-		System.out.println("This is Version 1.5");
-		System.out.println("Welcome to Version 1.5");
->>>>>>> parent of 461d3b5... Version 1.7
+		System.out.println("This is Version 1.8");
+		System.out.println("Welcome to Version 1.8");
 		
 		
 	}
